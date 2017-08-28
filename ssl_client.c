83b0ef87813813d84c845cb784047350a03fc143
a110605 <a1106052000@gmail.com>
2017-08-28 22:03:06 +0800

    change ssl method to sslv23

--- ssl_client.c
+++ ssl_client.c
@@ -43,7 +43,7 @@ SSL_CTX* InitCTX(void)
  
     OpenSSL_add_all_algorithms();  /* Load cryptos, et.al. */
     SSL_load_error_strings();   /* Bring in and register error messages */
-    method = SSLv3_client_method();  /* Create new client-method instance */
+    method = SSLv23_client_method();  /* Create new client-method instance */
     ctx = SSL_CTX_new(method);   /* Create new context */
     if ( ctx == NULL )
     {
